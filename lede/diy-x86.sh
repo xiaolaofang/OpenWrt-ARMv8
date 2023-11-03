4cc7b4074c9d6ca2e559c085023b282ef37d4ac1
xiaolaofang <70981892+xiaolaofang@users.noreply.github.com>
2023-11-03 22:22:15 +0800

    Update diy-x86.sh

--- lede/diy-x86.sh
+++ lede/diy-x86.sh
@@ -30,7 +30,7 @@ sed -i 's#luci-theme-bootstrap#luci-theme-opentomcat#g' feeds/luci/collections/l
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # Add additional packages
-git clone -b master --single-branch --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone -b main --single-branch --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 git clone -b main --single-branch --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
 git clone -b main --single-branch --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
 git clone -b main --single-branch --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
